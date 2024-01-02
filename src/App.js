57df9e4c0d0d8537a65c26d816cb91500c15208c
Ashutosh Taneja <obenkodehm@gmail.com>
2024-01-02 16:38:34 +0530

    Complete Project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import './App.css';
+import { useState } from "react";
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import SearchBar from './Navigation/SearchBar';
+import NavView from './Navigation/NavView';
+import Analytics from './Analytics/Analytics';
+import './App.scss';
+
+Chart.register(CategoryScale);
 
 function App() {
+  const [isNavViewOpen, toggleNavView] = useState(false);
+
   return (
-    <></>
+    <div id='App'>
+      <NavView isNavViewOpen={isNavViewOpen} />
+      <div className={(isNavViewOpen ? 'main-content':'main-content nav-closed')}>
+        <SearchBar isNavViewOpen={isNavViewOpen} toggleNavView={toggleNavView} />
+        <Analytics />
+      </div>
+    </div>
   );
 }
 
